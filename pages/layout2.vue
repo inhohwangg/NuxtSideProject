15476ae5f0530a8efa8d5f01b175c5cadbdbc581
inhohwangg <sk1440sk@naver.com>
2022-10-02 21:49:12 +0900

    layout work log

--- pages/layout2.vue
+++ pages/layout2.vue
@@ -1,5 +1,11 @@
 <template>
-  
+    <div class="main-background">
+        <h4 class="main-title">Buy Our Shoes!
+            <p style="margin: 0; color:bisque; font-size: 20px">신발이 필요하십니까?</p>
+            <p style="margin-top: 5px; color:bisque; font-size: 20px">아래의 버튼을 클릭해보세요!</p>
+        <button style="width:50px; margin-top: 10px; border-radius: 10px; background: aquamarine; cursor:pointer"><a href="https://naver.com" style="text-decoration:none">이동</a></button>
+        </h4>
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,18 @@ export default {
 </script>
 
 <style>
-
+.main-background {
+    width: 100%;
+    height: 500px;
+    background-image: url('https://codingapple.com/wp-content/uploads/2019/06/shoes.jpg'), url();
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: 1px;
+}
+.main-title {
+    color: white;
+    font-size: 40px;
+    margin-top: 100px;
+}
 </style>
