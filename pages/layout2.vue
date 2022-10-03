44fd2e031024ad3ff20b5a14a7ddd4d238593385
inhohwangg <sk1440sk@naver.com>
2022-10-03 18:03:19 +0900

    html test

--- pages/layout2.vue
+++ pages/layout2.vue
@@ -23,11 +23,34 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
     </div>
 
-    <br />
+    <div class="form-background">
+      <div class="form-white">
+        <form>
+          <div class="w-100">
+            <h3>Contack us</h3>
+            <p>Your Email</p>
+            <input
+              class="form-input"
+              type="email"
+              placeholder="email@example.com"
+            />
+          </div>
+          <div class="w-50">
+            <p>First Name</p>
+            <input class="form-input" type="text" />
+          </div>
+          <div class="w-50">
+            <p>Last Name</p>
+            <input class="form-input" type="text" />
+          </div>
+          <div style="clear: both"></div>
 
-    <div class="main2-title" style="padding: 50px">
-      <h4>How we design our shoes</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+          <div class="w-100">
+            <p>Message</p>
+            <textarea class="form-input"></textarea>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +65,10 @@ div {
   margin: 0px;
 }
 
+input,
+textarea {
+  box-sizing: border-box;
+}
 
 .main-background {
   width: 100%;
@@ -87,5 +114,38 @@ div {
   background-color: #eee;
   position: relative;
   top: -40px;
+}
+.text-input {
+  padding: 10px;
+  font-size: 20px;
+  border: 2px solid black;
+  border-radius: 5px;
+}
+.form-background {
+  background-color: black;
+  padding: 30px;
+}
+.form-white {
+  background-color: white;
+  padding: 30px;
+  width: 80%;
+  max-width: 600px;
+  margin: auto;
+}
+.form-input {
+  width: 100%;
+  padding: 10px;
+  font-size: 20px;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+.w-50 {
+  width: 50%;
+  float: left;
+  padding: 10px;
+}
+.w-100 {
+  width: 100%;
+  padding: 10px;
 }
 </style>
