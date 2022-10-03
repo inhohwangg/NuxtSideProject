821898e67624b298b8194e3428bc0900625339d2
inhohwangg <sk1440sk@naver.com>
2022-10-03 22:51:04 +0900

    add

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    <table>
-      <tr>
-        <th>1</th>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <p>dddd</p>
+              <p>dddd</p>
+            </th>
+            <td style="vertical-align: middle">2222</td>
+            <td>3333</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div>
+      <img
+        src="https://mdn.mozillademos.org/files/12245/frame_image.svg"
+        width="32"
+        height="32"
+      />
+      <span style="vertical-align: top">image with a default alignment.</span>
+    </div>
   </div>
 </template>
 
@@ -15,7 +38,11 @@ export default {};
 </script>
 
 <style>
-td, th {
+td,
+th {
   border: 1px solid black;
+}
+table {
+  border-collapse: collapse;
 }
 </style>
--- pages/header.vue
+++ pages/header.vue
@@ -7,18 +7,95 @@
           icon="search"
           style="margin-right: 15px; cursor: pointer"
         ></b-icon>
-        <button class="btn-style"><strong style="font-size: 16px">로그인</strong></button>
+        <button class="btn-style">
+          <strong style="font-size: 16px">로그인</strong>
+        </button>
       </div>
     </div>
-    <div class="container3">
-
+    <div class="container3"></div>
+    <div>
+      <input type="text" placeholder="kakaotest" />
+      <button
+        @click="kakaotest"
+        style="
+          padding: 5px;
+          border: none;
+          border-radius: 5px;
+          background: yellowgreen;
+          cursor: pointer;
+        "
+      >
+        전송
+      </button>
+    </div>
+    <div>
+      <input v-model="title" type="text" placeholder="postTest-title" />
+      <input v-model="content" type="text" placeholder="postTest-content" />
+      <button
+        @click="postWrite"
+        style="
+          padding: 5px;
+          border: none;
+          border-radius: 5px;
+          background: yellowgreen;
+          cursor: pointer;
+        "
+      >
+        전송
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 import { IconsPlugin } from "bootstrap-vue";
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      title:'',
+      content:'',
+    }
+  },
+  methods: {
+    kakaotest() {
+      const API_KEY = process.env.KAKAO_AUTH_KEY;
+      const data = 'a4f7f774f4ffb1fe8d4a59f3759ea520'
+      const REDIRECT_URI = "http://localhost:3000/oauth";
+      const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+      axios
+        .get(KAKAO_AUTH_URI, {headers: data})
+        .then((res) => {
+          console.log(res);
+          console.log("뭐라고나오려나....");
+        })
+        .catch((error) => {
+          console.log(error, "실패!!");
+        });
+    },
+    postWrite() {
+      const host = "http://13.125.96.150:3000/api/post/write";
+      let token = localStorage.getItem("access_token");
+      let data = {
+        title: this.title,
+        content: this.content,
+      };
+      axios
+        .post(host, data, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          let token = localStorage.getItem("access_token");
+          localStorage.setItem("access_token", token);
+          console.log(res.data);
+          console.log("게시글 작성 성공");
+        })
+        .catch((error) => {
+          console.log(error, "게시글 작성 실패!");
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -50,8 +127,8 @@ export default {};
   cursor: pointer;
 }
 .container3 {
-    width: 90%;
-    margin-top: 30px;
-    background-color: green;
+  width: 90%;
+  margin-top: 30px;
+  background-color: green;
 }
 </style>
